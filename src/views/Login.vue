2e6e26a431968c01c8bfcd1aa2e0b29de3eb2834
杨泽 <1083849984@qq.com>
2021-02-05 17:58:55 +0800

    登录框输入汉字并提示错误信息

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="formWrapper">
     <h1>后台管理系统</h1>
-    <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" class="demo-ruleForm" label-position="left">
+    <el-form :model="ruleForm" status-icon  ref="ruleForm" class="demo-ruleForm" label-position="left">
       <el-form-item label="用户名" prop="username" label-width="55px">
-        <el-input type="password" v-model="ruleForm.username" autocomplete="off"></el-input>
+        <el-input v-model="ruleForm.username" autocomplete="off"></el-input>
       </el-form-item>
       <el-form-item label="密码" prop="password" label-width="50px">
-        <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
+        <el-input v-model="ruleForm.password" autocomplete="off" show-password></el-input>
       </el-form-item>
       <el-form-item class="commitButton" style="margin-left: 0">
         <el-button type="primary" @click="submitForm('ruleForm')" style="width:100%">提交</el-button>
@@ -20,50 +20,25 @@ import {setToken} from '@/utils/auth'
 
 export default {
   data () {
-    let checkUserName = (rule, value, callback) => {
-      if (value === '') {
-        callback(new Error('请输入用户名'))
-      } else {
-        callback()
-      }
-    }
-    let checkPassword = (rule, value, callback) => {
-      if (value === '') {
-        callback(new Error('请输入密码'))
-      } else {
-        callback()
-      }
-    }
     return {
       ruleForm: {
         username: 'admin',
         password: '123456'
       },
-      rules: {
-        username: [
-          {validator: checkUserName, trigger: 'blur'}
-        ],
-        password: [
-          {validator: checkPassword, trigger: 'blur'}
-        ]
-      }
     }
   },
   methods: {
-    submitForm (formName) {
-      this.$refs[formName].validate((valid) => {
-        if (valid) {
-          this.$store.dispatch('login', this.ruleForm)
-            .then(data => {
-              if (data.info.result === 'success') {
-                setToken()
-                router.push('/layout')
-              }
-            })
-        } else {
-          console.log('error submit!!')
-          return false
-        }
+    submitForm () {
+      this.$store.dispatch('login', this.ruleForm)
+        .then(data => {
+          if (data.info.result === 'success') {
+            setToken()
+            router.push('/layout')
+          }  else {
+            throw new Error('请重新输入用户名或密码')
+          }
+        }).catch(err => {
+        this.$message({type: 'info', message: err})
       })
     }
   }
